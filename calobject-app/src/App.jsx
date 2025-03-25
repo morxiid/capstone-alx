26bfe9259ffbb2b22646afa618aeb39e5ebc17be
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-03-25 02:13:32 +0000

    add dynamic routing

--- calobject-app/src/App.jsx
+++ calobject-app/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Route, Routes, Link} from 'react-router-dom'
+import React from "react";
+import HomePage from './components/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import LogInForm from './components/LoginForm';
+import SignUpForm from './components/SignupForm';
+import WebApp from './components/WebApp';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const[isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    //Recive user data from local storage
+    const storedData = JSON.parse(localStorage.getItem("user"));
+
+    //Get only username from local storage 
+    const username = storedData ? storedData.username : "User";
+
+    //check local storage on first load
+    useEffect(() => {
+      if(storedData){
+        setIsLoggedIn(true)
+      }
+    }, []);
+    
+    
+    //Remove user from local storage when log out
+    function onLogout(){
+      localStorage.removeItem("user");
+      setIsLoggedIn(false);
+      navigate("/");
+    }
+
+    return (
+      <>
+        <Header username={username} isLoggedIn={isLoggedIn} onLogout={onLogout}/>
+        <Routes>
+          <Route path='/' element = {<HomePage/>} />
+          <Route path='/signup' element = {<SignUpForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
+          <Route path='/login' element = {<LogInForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
+          <Route path='/calobject' element = {<WebApp/>} />
+        </Routes>
+        <Footer />
+      </>
+    )
 }
 
 export default App
+
